4a7052e5f9526ca3e534c030b0e875dd786df9fc
willslaq <willslaqtomaz@gmail.com>
2020-06-16 18:08:08 -0300

    Subindo botão de cadastro standalone e balão css

--- frontend/src/screens/home/index.js
+++ frontend/src/screens/home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from '../../components/Navbar/index';
 import Container from '@material-ui/core/Container';
+import AddPesoaButton from '../../components/AddPessoaButton/index';
+import { makeStyles } from '@material-ui/core/styles';
 
 // import FreeSoloCreateOptionDialog from '../../components/pesquisa/index';
 import FreeSoloCreateOptionDialog from '../../components/Pesquisa/cloneDebug';
@@ -8,10 +10,52 @@ import FreeSoloCreateOptionDialog from '../../components/Pesquisa/cloneDebug';
 // import CardPessoa from '../../components/CardPessoa/index';
 import CardPessoa from '../../components/CardPessoa/cloneDebug';
 
+const useStyles = makeStyles(theme => ({
+    controlaButton: {
+        position: "fixed"
+    },
+    dica: {
+        margin: '0 auto',
+        background: '#7e57c2',
+        fontFamily: 'open sans',
+        fontSize: '0.85em',
+        lineHeight: '1.6em',
+        borderRadius: '15px',
+        width: '300px',
+        height: 'auto',
+        color: '#fff',
+        padding: '20px',
+        position: 'relative',
+        marginTop: '30px',
+        '&::after': {
+            content: "",
+            width: 0,
+            height: 0,
+            position: 'absolute',
+            borderLeft: '20px solid transparent',
+            borderRight: '20px solid transparent',
+            /*Faz seta "apontar para baixo. Definir o valor como 'top' fará ela "apontar para cima" */
+            /*Aqui entra a cor da "aba" do balão */
+            borderBottom: '20px solid #7e57c2',
+            top: '-20px', /*localização. Experimente alterar para 'bottom'*/
+            left: '20%',
+        }
+    }
+}))
+
 export default function Home() {
+
+    const classes = useStyles();
+
     return (
         <>
             <NavBar />
+            <div className={classes.controlaButton}>
+                <AddPesoaButton />
+                <div className={classes.dica}>
+                    Utilize o botão acima para cadastrar uma nova pessoa.
+                </div>
+            </div>
             <Container maxWidth="md" style={{ marginTop: 10 }}>
                 {/* <FreeSoloCreateOptionDialog /> */}
                 <CardPessoa />
